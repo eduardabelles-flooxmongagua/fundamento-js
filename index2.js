95fa7b0455bd3a45b6ca6b017c673bc751617305
eduardabelles-flooxmongagua <eduarda.belles.flooxmongagua@gmail.com>
2025-02-21 12:53:53 -0300

    feat: add index2.js file with switch and for loop examples

---
+++ index2.js
@@ -0,0 +1,28 @@
+//example switch
+
+let cor="verde";
+
+switch(cor){
+   case "verde":
+   console.log("siga");
+   break;
+
+   case "amarelo":
+   console.log("atenção");
+   break;
+
+   case "vermelho":
+   console.log("vermelho");
+   break;
+
+   default:
+    console.log("não sei");
+
+}
+
+//Multiplication Table of 5
+let n=5;
+for(let i=0; i<=10;i++){
+    console.log(`${i} X ${n} = ${i*n}`);
+    //console.log(i+"X"+n+"="+(i*n));
+}
