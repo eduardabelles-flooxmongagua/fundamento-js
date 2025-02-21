1079f9e0e0b5d158b6b2c3d79d7eca55752c8885
eduardabelles-flooxmongagua <eduarda.belles.flooxmongagua@gmail.com>
2025-02-21 12:06:46 -0300

    feat: add variables,conditions and operators

--- index.js
+++ index.js
@@ -3,3 +3,46 @@ alert("Hello World!");
 // This is a short comment
 /* This is a long comment */
 
+var olaMundo="Olá Mundo!!";
+console.log(olaMundo);
+
+//variable types var, let(scope), const(constant)
+
+//= assignment
+let a=10;
+const b="10";
+//console.log(a=b); 
+//console.log(a==b && typeof a ==='string');
+console.log(a==b || typeof a ==='string');
+
+//== comparasion
+let c="20";
+const d=20;
+console.log(c==d);
+
+//=== value and data type comparison
+let e="20";
+const f=20;
+console.log(c===d);
+
+//!= different
+let g="20";
+const h=20;
+console.log(c!=d);
+
+//If & else
+let cor = "amarelo";
+//let cor ="vermelho";
+//let cor ="verde";
+
+if(cor==="vermelho"){
+    console.log("siga");
+}
+else if(cor==="amarelo"){
+    console.log("atenção");
+}
+else{
+    console.log("pare");
+}
+
+
