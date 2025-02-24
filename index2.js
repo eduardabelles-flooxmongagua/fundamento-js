c3072d104034eb1506cc1a9bde1eae37bd034cf5
eduardabelles-flooxmongagua <eduarda.belles.flooxmongagua@gmail.com>
2025-02-24 05:35:22 -0300

    feat: add functions, anonymous functions, and arrow functions.

--- index2.js
+++ index2.js
@@ -26,3 +26,34 @@ for(let i=0; i<=10;i++){
     console.log(`${i} X ${n} = ${i*n}`);
     //console.log(i+"X"+n+"="+(i*n));
 }
+
+//fuctions
+
+function soma(x1,x2){
+return x1+x2;
+}
+let resultado=soma(1,2);
+console.log(resultado);
+
+/*function calc(x1,x2,operator){
+    return eval(`${x1} ${operator} ${x2}`);
+    }
+    let result=calc(6,2,"+");
+    console.log("Soma="+result);
+*/
+    //anonymous fuction
+    console.log(function (x1,x2,operator){
+        return eval(`${x1} ${operator} ${x2}`);
+    }(8,2,"+"));
+        
+//Arrow function
+let calc = (v1,v2,operator)=>{
+
+    return eval(`${v1} ${operator} ${v2}`);
+
+    }
+    let result=calc(6,2,"+");
+    console.log("Soma="+result);
+
+
+    
